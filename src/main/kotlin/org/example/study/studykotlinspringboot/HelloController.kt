542b12fa00c9c5f31bf2e0803dd45cfbe4d4ca6a
alkaline2018 <plus159@>
2025-08-20 17:18:10 +0900

    remove: HelloController에서 hello2 API 제거
    - 사용되지 않는 hello2 API 메서드 삭제
    - 코드 간소화 및 유지보수성 개선을 위해 불필요한 코드 제거

--- src/main/kotlin/org/example/study/studykotlinspringboot/HelloController.kt
+++ src/main/kotlin/org/example/study/studykotlinspringboot/HelloController.kt
@@ -10,9 +10,6 @@ class HelloController {
     fun hello(@RequestParam name: String = "world") =
         mapOf("message" to "Hello, $name")
 
-    @GetMapping("/api/hello2")
-    fun hello2(@RequestParam name: String = "world") =
-        mapOf("message" to "Hello2, $name")
 
     @GetMapping("/api/add")
     fun add(@RequestParam number1: Int, @RequestParam number2: Int): Map<String, Any> {

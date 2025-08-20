c2336c3eefd9bbd5d8fc6f790904b974b291d878
alkaline2018 <plus159@>
2025-08-20 17:50:58 +0900

    remove: HelloController에서 add API 제거
    - 사용되지 않는 add API 메서드 삭제
    - 코드 간소화 및 유지보수성 개선을 위해 불필요한 코드 제거

--- src/main/kotlin/org/example/study/studykotlinspringboot/HelloController.kt
+++ src/main/kotlin/org/example/study/studykotlinspringboot/HelloController.kt
@@ -9,11 +9,4 @@ class HelloController {
     @GetMapping("/api/hello")
     fun hello(@RequestParam name: String = "world") =
         mapOf("message" to "Hello, $name")
-
-
-    @GetMapping("/api/add")
-    fun add(@RequestParam number1: Int, @RequestParam number2: Int): Map<String, Any> {
-        val number3: Int = number1 + number2
-        return mapOf("message" to number3)
-    }
 }
